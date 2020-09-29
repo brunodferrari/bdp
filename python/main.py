83d839e9bcc26378fb455e09d2c11adc8e396c49
Bruno Ferrari <bferrari@absoluteinvest.com.br>
2020-09-29 10:52:12 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -32,6 +32,8 @@ B.add_nodes_from(["a", "b", "c"], bipartite=2)
 B.add_edges_from([(5, "a"), (5, "b"), (2, "b"), (2, "c"), (3, "c"), (4, "a")])
 
 
+
+
 def pi(setlist, i):
     try:
         return np.int(np.where(np.array(setlist) == i )[0])
@@ -40,24 +42,31 @@ def pi(setlist, i):
 
 def plotBGraph(G):
     
-    top = list(nx.bipartite.sets(G)[0])[::-1]
-    #top = nx.bipartite.sets(G)[0]
-    pos = nx.bipartite_layout(G, top)
+    B = nx.Graph()
+    B.add_nodes_from(G.v1(), bipartite=1)
+    B.add_nodes_from(G.v2(), bipartite=2)
+    B.add_edges_from(G.edges())
     
-    nx.draw(G, pos)
-    nx.draw_networkx_labels(G, pos)
+    top = list(nx.bipartite.sets(B)[0])[::-1]
+    #top = nx.bipartite.sets(G)[0]
+    pos = nx.bipartite_layout(B, top)    
+    nx.draw(B, pos)
+    nx.draw_networkx_labels(B,pos)
+
 
 
 class BGraph:
     
-    #def __init__(self):
-     #   self.set_v1 = []
-
-    def set_v1(self):
+    def __init__(self):
         self.set_v1 = []
-    
-    def set_v2(self):
         self.set_v2 = []
+        self.set_edges = []
+            
+    def edges(self, edgelist = None):
+        if edgelist != None:
+            self.set_edges = []
+            self.set_edges = edgelist
+        return self.set_edges
     
     def v1 (self, setlist = None):
         if setlist != None:
@@ -81,19 +90,19 @@ class BGraph:
         if pos != None:
             return np.vectorize(lambda i: pi(self.v2(), i))(pos)
         else:
-            return np.vectorize(lambda i: pi(self.v1(), i))(self.v2())
+            return np.vectorize(lambda i: pi(self.v2(), i))(self.v2())
     
     def add_v1(self, i, pos):
         if pos != -1: 
-            self.set_v1 = C.v1()[:pos] + [i] + C.v1()[pos:]
+            self.set_v1 = self.v1()[:pos] + [i] + self.v1()[pos:]
         else:
-            self.set_v1 = C.v1()[:] + [i]
+            self.set_v1 = self.v1()[:] + [i]
         
     def add_v2(self, i, pos):
         if pos != -1: 
-            self.set_v2 = C.v2()[:pos] + [i] + C.v1()[pos:]
+            self.set_v2 = self.v2()[:pos] + [i] + self.v2()[pos:]
         else:
-            self.set_v2 = C.v2()[:] + [i]
+            self.set_v2 = self.v2()[:] + [i]
     
     def n_v1(self):
         #self.n_v1 = len(self.v1)
@@ -108,6 +117,9 @@ class BGraph:
         
     def adj_v1(self, G):
         self.adj_v1 = G
-
-C = BGraph()
-C.v1([10,3,4,5,61, 20])
+        
+B = BGraph()
+B.v1([5,3,2,4])
+B.v2(['a','b','c'])
+B.edges([(5, "a"), (5, "b"), (2, "b"), (2, "c"), (3, "c"), (4, "a")])
+plotBGraph(B)
