fd76635cf1493d7fce0bed156c09e9f6b44d99cd
Bruno Ferrari <bferrari@absoluteinvest.com.br>
2021-03-30 09:56:34 -0300

    Update grasp.py

--- python/grasp.py
+++ python/grasp.py
@@ -45,15 +45,6 @@ for row in graph_data.iloc[1:32, 0]:
         if int(adj) not in graph_adj_nodes_incre: 
             graph_edges2.append(( int(row.split(" ")[1]), int(adj)))
 
-New = bgraph.BGraph()
-New.v2(np.unique(np.array(np.matrix(graph_edges)[:,1])).tolist())
-New.v1(np.unique(np.array(np.matrix(graph_edges)[:,0])).tolist())
-New.edges(graph_edges)
-bgraph.plotBGraph(New)
-plt.title("N crossing: "+ str(bgraph.crossing(New)) )
-plt.show()
-
-
 def greedy_selection(G, alpha=1.0, nodelist = None, subgraph = None):
     
     deg_dict = G.degree(nodelist, subgraph)
@@ -64,6 +55,93 @@ def greedy_selection(G, alpha=1.0, nodelist = None, subgraph = None):
     
     return v
 
+def construction_phase(G, U_1, U_2, V, alpha=1.0):
+   
+    v = greedy_selection(New)
+    try:
+        U_1.remove(v)
+        New.pi_1[v] = 1
+    except ValueError:
+        U_2.remove(v)
+        New.pi_2[v] = 1
+    
+    a = alpha #alpha values
+    while len(U_1 + U_2) > 0:
+        v = greedy_selection(New, a, U_1+U_2, list(set(V) - set(U_1+U_2))  )    
+    
+        try:
+            U_1.remove(v)
+            bc_v = New.bc(v, k=1)
+            
+            fbc_v = np.floor(bc_v).astype(int)
+            cbc_v = np.ceil(bc_v).astype(int)
+            
+            pos_assing = list(New.pi_1.values())
+            
+            f_ncross = np.inf
+            c_ncross = np.inf
+            
+            if fbc_v not in pos_assing:
+                New.pi_1[v] = fbc_v
+                f_ncross = New.n_cross()
+            elif fbc_v - 1 > 0:
+                fbc_v = fbc_v - 1
+                New.pi_1[v] = fbc_v
+                f_ncross = New.n_cross()
+                
+            if cbc_v not in pos_assing:
+                New.pi_1[v] = cbc_v
+                c_ncross = New.n_cross()
+            elif cbc_v + 1 < New.n_v1():
+                cbc_v = cbc_v + 1
+                New.pi_1[v] = cbc_v
+                c_ncross = New.n_cross()
+            #verificar se é necessaria essa parte
+            pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
+            if pos in pos_assing: pos = max(pos_assing) + 1
+            New.pi_1[v] = pos
+            
+        except ValueError:
+            U_2.remove(v)
+            bc_v = New.bc(v, k=2)
+            
+            fbc_v = np.floor(bc_v).astype(int)
+            cbc_v = np.ceil(bc_v).astype(int)
+            
+            pos_assing = list(New.pi_2.values())
+            
+            f_ncross = np.inf
+            c_ncross = np.inf
+            if fbc_v not in pos_assing:
+                New.pi_2[v] = fbc_v
+                f_ncross = New.n_cross()
+            elif fbc_v - 1 > 0:
+                fbc_v = fbc_v - 1
+                New.pi_2[v] = fbc_v
+                f_ncross = New.n_cross()
+                
+            if cbc_v not in pos_assing:
+                New.pi_2[v] = cbc_v
+                c_ncross = New.n_cross()
+            elif cbc_v + 1 < New.n_v2():
+                cbc_v = cbc_v + 1
+                New.pi_2[v] = cbc_v
+                c_ncross = New.n_cross()
+            #verificar se é necessaria essa parte
+            pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
+            if pos in pos_assing: pos = max(pos_assing) + 1
+            New.pi_2[v] = pos
+
+
+
+New = bgraph.BGraph()
+New.v2(np.unique(np.array(np.matrix(graph_edges)[:,1])).tolist())
+New.v1(np.unique(np.array(np.matrix(graph_edges)[:,0])).tolist())
+New.edges(graph_edges)
+bgraph.plotBGraph(New)
+plt.title("N crossing: "+ str(bgraph.crossing(New)) )
+plt.show()
+
 U_1 = New.v1().copy()
 U_2 = New.v2().copy()
 V = U_1 + U_2
@@ -72,82 +150,7 @@ V = U_1 + U_2
 New.pi_1 = dict(zip(U_1, New.n_v1() * [0]))
 New.pi_2 = dict(zip(U_2, New.n_v2() * [0]))
 
-v = greedy_selection(New)
-    
-try:
-    U_1.remove(v)
-    New.pi_1[v] = 1
-except ValueError:
-    U_2.remove(v)
-    New.pi_2[v] = 1
-
-a = 1.0 #alpha values
-while len(U_1 + U_2) > 0:
-    v = greedy_selection(New, a, U_1+U_2, list(set(V) - set(U_1+U_2))  )    
-
-    try:
-        U_1.remove(v)
-        bc_v = New.bc(v, k=1)
-        
-        fbc_v = np.floor(bc_v).astype(int)
-        cbc_v = np.ceil(bc_v).astype(int)
-        
-        pos_assing = list(New.pi_1.values())
-        
-        f_ncross = np.inf
-        c_ncross = np.inf
-        
-        if fbc_v not in pos_assing:
-            New.pi_1[v] = fbc_v
-            f_ncross = New.n_cross()
-        elif fbc_v - 1 > 0:
-            fbc_v = fbc_v - 1
-            New.pi_1[v] = fbc_v
-            f_ncross = New.n_cross()
-            
-        if cbc_v not in pos_assing:
-            New.pi_1[v] = cbc_v
-            c_ncross = New.n_cross()
-        elif cbc_v + 1 < New.n_v1():
-            cbc_v = cbc_v + 1
-            New.pi_1[v] = cbc_v
-            c_ncross = New.n_cross()
-        #verificar se é necessaria essa parte
-        pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
-        if pos in pos_assing: pos = max(pos_assing) + 1
-        New.pi_1[v] = pos
-        
-    except ValueError:
-        U_2.remove(v)
-        bc_v = New.bc(v, k=2)
-        
-        fbc_v = np.floor(bc_v).astype(int)
-        cbc_v = np.ceil(bc_v).astype(int)
-        
-        pos_assing = list(New.pi_2.values())
-        
-        f_ncross = np.inf
-        c_ncross = np.inf
-        if fbc_v not in pos_assing:
-            New.pi_2[v] = fbc_v
-            f_ncross = New.n_cross()
-        elif fbc_v - 1 > 0:
-            fbc_v = fbc_v - 1
-            New.pi_2[v] = fbc_v
-            f_ncross = New.n_cross()
-            
-        if cbc_v not in pos_assing:
-            New.pi_2[v] = cbc_v
-            c_ncross = New.n_cross()
-        elif cbc_v + 1 < New.n_v2():
-            cbc_v = cbc_v + 1
-            New.pi_2[v] = cbc_v
-            c_ncross = New.n_cross()
-        #verificar se é necessaria essa parte
-        pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
-        if pos in pos_assing: pos = max(pos_assing) + 1
-        New.pi_2[v] = pos
-
+construction_phase(New, U_1, U_2, V, 1)
 New.order_v1()
 New.order_v2()
 bgraph.plotBGraph(New)
