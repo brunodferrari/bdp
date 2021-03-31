6224eb066df773722cb623cc08743aa1e92c05cf
Bruno Ferrari <bferrari@absoluteinvest.com.br>
2021-03-31 10:34:54 -0300

    ajuste codigo

--- python/grasp.py
+++ python/grasp.py
@@ -3,9 +3,7 @@
 Created on Sat Mar 27 17:24:59 2021
 
 @author: Bruno Ferrari
-"""
 
-'''
 The GRASPH META-HEURISTC for bdp works in the two main phases:
     -> Construction Phase:
         Create 2 list of unassigned vertices, with U_1 + U_2 = U = V
@@ -19,17 +17,19 @@ The GRASPH META-HEURISTC for bdp works in the two main phases:
     -> Improvement Phase
 
 
-'''
-%cd C:\Users\bferrari\Desktop\pessoal\bdp\
+"""
+#%cd C:\Users\bferrari\Desktop\pessoal\bdp\
 
+import random
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import networkx as nx
-import random
 
-from python import bgraph
-graph_data = pd.read_csv("./dbdp_instances/instances/incgraph_25_25_0.3_0.2_1.txt")
+
+#from python 
+import bgraph
+graph_data = pd.read_csv("C:/Users/bferrari/Desktop/pessoal/bdp/dbdp_instances/instances/incgraph_25_25_0.3_0.2_2.txt")
 
 graph_adj_nodes_incre = graph_data.iloc[-6:,0].str.split(" ",expand=True).iloc[:, 1].astype(int).to_list()
 
@@ -45,7 +45,7 @@ for row in graph_data.iloc[1:32, 0]:
         if int(adj) not in graph_adj_nodes_incre: 
             graph_edges2.append(( int(row.split(" ")[1]), int(adj)))
 
-def greedy_selection(G, alpha=1.0, nodelist = None, subgraph = None):
+def greedy_selection(G, alpha=1.0, nodelist=None, subgraph=None):
     
     deg_dict = G.degree(nodelist, subgraph)
     deg_max = max(deg_dict.values())
@@ -57,154 +57,154 @@ def greedy_selection(G, alpha=1.0, nodelist = None, subgraph = None):
 
 def construction_phase(G, U_1, U_2, V, alpha=1.0):
    
-    v = greedy_selection(New)
+    v = greedy_selection(G)
     try:
         U_1.remove(v)
-        New.pi_1[v] = 1
+        G.pi_1[v] = 1
     except ValueError:
         U_2.remove(v)
-        New.pi_2[v] = 1
+        G.pi_2[v] = 1
     
     a = alpha #alpha values
     while len(U_1 + U_2) > 0:
-        v = greedy_selection(New, a, U_1+U_2, list(set(V) - set(U_1+U_2))  )    
+        v = greedy_selection(G, a, U_1+U_2, list(set(V) - set(U_1+U_2))  )    
     
         try:
             U_1.remove(v)
-            bc_v = New.bc(v, k=1)
+            bc_v = G.bc(v, k=1)
             
             fbc_v = np.floor(bc_v).astype(int)
             cbc_v = np.ceil(bc_v).astype(int)
             
-            pos_assing = list(New.pi_1.values())
+            pos_assing = list(G.pi_1.values())
             
             f_ncross = np.inf
             c_ncross = np.inf
             
             if fbc_v not in pos_assing:
-                New.pi_1[v] = fbc_v
-                f_ncross = New.n_cross()
+                G.pi_1[v] = fbc_v
+                f_ncross = G.n_cross()
             elif fbc_v - 1 > 0:
                 fbc_v = fbc_v - 1
-                New.pi_1[v] = fbc_v
-                f_ncross = New.n_cross()
+                G.pi_1[v] = fbc_v
+                f_ncross = G.n_cross()
                 
             if cbc_v not in pos_assing:
-                New.pi_1[v] = cbc_v
-                c_ncross = New.n_cross()
-            elif cbc_v + 1 < New.n_v1():
+                G.pi_1[v] = cbc_v
+                c_ncross = G.n_cross()
+            elif cbc_v + 1 < G.n_v1():
                 cbc_v = cbc_v + 1
-                New.pi_1[v] = cbc_v
-                c_ncross = New.n_cross()
+                G.pi_1[v] = cbc_v
+                c_ncross = G.n_cross()
             #verificar se é necessaria essa parte
             pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
             if pos in pos_assing: pos = max(pos_assing) + 1
-            New.pi_1[v] = pos
+            G.pi_1[v] = pos
             
         except ValueError:
             U_2.remove(v)
-            bc_v = New.bc(v, k=2)
+            bc_v = G.bc(v, k=2)
             
             fbc_v = np.floor(bc_v).astype(int)
             cbc_v = np.ceil(bc_v).astype(int)
             
-            pos_assing = list(New.pi_2.values())
+            pos_assing = list(G.pi_2.values())
             
             f_ncross = np.inf
             c_ncross = np.inf
             if fbc_v not in pos_assing:
-                New.pi_2[v] = fbc_v
-                f_ncross = New.n_cross()
+                G.pi_2[v] = fbc_v
+                f_ncross = G.n_cross()
             elif fbc_v - 1 > 0:
                 fbc_v = fbc_v - 1
-                New.pi_2[v] = fbc_v
-                f_ncross = New.n_cross()
+                G.pi_2[v] = fbc_v
+                f_ncross = G.n_cross()
                 
             if cbc_v not in pos_assing:
-                New.pi_2[v] = cbc_v
-                c_ncross = New.n_cross()
-            elif cbc_v + 1 < New.n_v2():
+                G.pi_2[v] = cbc_v
+                c_ncross = G.n_cross()
+            elif cbc_v + 1 < G.n_v2():
                 cbc_v = cbc_v + 1
-                New.pi_2[v] = cbc_v
-                c_ncross = New.n_cross()
+                G.pi_2[v] = cbc_v
+                c_ncross = G.n_cross()
             #verificar se é necessaria essa parte
             pos = min( [(c_ncross, cbc_v),(f_ncross, fbc_v)]  )[1]
             if pos in pos_assing: pos = max(pos_assing) + 1
-            New.pi_2[v] = pos
+            G.pi_2[v] = pos
 
 def improvement_phase(G, U_1, U_2, V):
-    Pr_list = list(New.degree().values()) / np.array(list(New.degree().values())).sum()
-    Pr_dict = dict(zip(New.degree().keys(),  Pr_list) ) #vetor de probabilidades
+    Pr_list = list(G.degree().values()) / np.array(list(G.degree().values())).sum()
+    Pr_dict = dict(zip(G.degree().keys(),  Pr_list) ) #vetor de probabilidades
     
     while len(U_1 + U_2) > 0:
     
         v = random.choices(list(Pr_dict.keys()),weights=list(Pr_dict.values()))[0]
         Pr_dict.pop(v)
-        cross_i = New.n_cross()
-        print(cross_i)
+        cross_i = G.n_cross()
+        print("move v: ", v, " n_cross: ", cross_i)
         try:
             U_1.remove(v)
-            bc_v = New.bc(v, k=1)
+            bc_v = G.bc(v, k=1)
                
             fbc_v = np.floor(bc_v).astype(int)
             cbc_v = np.ceil(bc_v).astype(int)
-            pi_aux = New.pi_1.copy()
+            pi_aux = G.pi_1.copy()
         
-            pi_f = New.move_v1(v, fbc_v)
-            New.pi_1 = pi_f
-            f_ncross =  New.n_cross()
+            pi_f = G.move_v1(v, fbc_v)
+            G.pi_1 = pi_f
+            f_ncross =  G.n_cross()
             
-            pi_c = New.move_v1(v, cbc_v)
-            New.pi_1 = pi_c
-            c_ncross = New.n_cross()
+            pi_c = G.move_v1(v, cbc_v)
+            G.pi_1 = pi_c
+            c_ncross = G.n_cross()
             
             f_1_ncross = np.inf
             pi_f_1 = pi_aux
             if fbc_v - 1 > 0:
-                pi_f_1 = New.move_v1(v, fbc_v-1)
-                New.pi_1 = pi_f_1
-                f_1_ncross = New.n_cross()
+                pi_f_1 = G.move_v1(v, fbc_v-1)
+                G.pi_1 = pi_f_1
+                f_1_ncross = G.n_cross()
             
             c_1_ncross = np.inf
             pi_c_1 = pi_aux
-            if cbc_v + 1 < New.n_v1():
-                pi_c_1 = New.move_v1(v, cbc_v + 1)
-                New.pi_1 = pi_c_1
-                c_1_ncross = New.n_cross()
+            if cbc_v + 1 < G.n_v1():
+                pi_c_1 = G.move_v1(v, cbc_v + 1)
+                G.pi_1 = pi_c_1
+                c_1_ncross = G.n_cross()
             
-            New.pi_1 = min( [(c_ncross, pi_c),(f_ncross, pi_f), \
+            G.pi_1 = min( [(c_ncross, pi_c),(f_ncross, pi_f), \
                              (f_1_ncross, pi_f_1), (c_1_ncross, pi_c_1), (cross_i, pi_aux)], key=lambda x: x[0])[1]
         except ValueError:
             U_2.remove(v)
-            bc_v = New.bc(v, k=2)
+            bc_v = G.bc(v, k=2)
                
             fbc_v = np.floor(bc_v).astype(int)
             cbc_v = np.ceil(bc_v).astype(int)
-            pi_aux = New.pi_2.copy()
+            pi_aux = G.pi_2.copy()
         
-            pi_f = New.move_v2(v, fbc_v)
-            New.pi_2 = pi_f
-            f_ncross =  New.n_cross()
+            pi_f = G.move_v2(v, fbc_v)
+            G.pi_2 = pi_f
+            f_ncross =  G.n_cross()
             
-            pi_c = New.move_v2(v, cbc_v)
-            New.pi_2 = pi_c
-            c_ncross = New.n_cross()
+            pi_c = G.move_v2(v, cbc_v)
+            G.pi_2 = pi_c
+            c_ncross = G.n_cross()
             
             f_1_ncross = np.inf
             pi_f_1 = pi_aux
             if fbc_v - 1 > 0:
-                pi_f_1 = New.move_v2(v, fbc_v-1)
-                New.pi_2 = pi_f_1
-                f_1_ncross = New.n_cross()
+                pi_f_1 = G.move_v2(v, fbc_v-1)
+                G.pi_2 = pi_f_1
+                f_1_ncross = G.n_cross()
             
             c_1_ncross = np.inf
             pi_c_1 = pi_aux
-            if cbc_v + 1 < New.n_v2():
-                pi_c_1 = New.move_v2(v, cbc_v + 1)
-                New.pi_2 = pi_c_1
-                c_1_ncross = New.n_cross()
+            if cbc_v + 1 < G.n_v2():
+                pi_c_1 = G.move_v2(v, cbc_v + 1)
+                G.pi_2 = pi_c_1
+                c_1_ncross = G.n_cross()
             
-            New.pi_2 = min( [(c_ncross, pi_c), (f_ncross, pi_f), \
+            G.pi_2 = min( [(c_ncross, pi_c), (f_ncross, pi_f), \
                              (f_1_ncross, pi_f_1), (c_1_ncross, pi_c_1), (cross_i, pi_aux)], key=lambda x: x[0] )[1]
     
 
@@ -233,15 +233,27 @@ plt.title("N crossing: "+ str(bgraph.crossing(New)) )
 plt.show()
 
 
-U_1 = New.v1().copy()
-U_2 = New.v2().copy()
 
-improvement_phase(New, U_1, U_2, V)
-New.order_v1()
-New.order_v2()
-bgraph.plotBGraph(New)
-plt.title("N crossing: "+ str(bgraph.crossing(New)) )
-plt.show()
+
+min_cross_ant = New.n_cross()
+min_cross = min_cross_ant - 1
+while min_cross < min_cross_ant:
+    
+    U_1 = New.v1().copy()
+    U_2 = New.v2().copy()   
+    
+    improvement_phase(New, U_1, U_2, V)
+    New.order_v1()
+    New.order_v2()
+    
+    min_cross_ant = min_cross
+    min_cross = bgraph.crossing(New)
+    
+    
+    bgraph.plotBGraph(New)
+    plt.title("N crossing: "+ str(min_cross) )
+    plt.show()
+    
 # New.degree(U_linha, [2])
 
 # G = nx.bipartite.gnmk_random_graph(3,5,10, seed=111)
